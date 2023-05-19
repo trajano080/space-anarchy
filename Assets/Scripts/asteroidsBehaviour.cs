e761ccba2a682da460046ba28ab937fba6a68e8f
Valentin Lopez <valentinlopezandres@gmail.com>
2023-05-19 13:51:16 +0200

    Create first asteroid and make it move towards the ship

---
+++ Assets/Scripts/asteroidsBehaviour.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class asteroidsBehaviour : MonoBehaviour
+{
+    public float asteroidSpeed = 1.0f;
+    void Update()
+    {
+        transform.Translate(Vector3.back * Time.deltaTime * asteroidSpeed, Space.World);
+    }
+}
