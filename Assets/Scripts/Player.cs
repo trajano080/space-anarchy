fdde89f3eb92f2fb59869ff660e8f3367f0e3582
Valentin Lopez <valentinlopezandres@gmail.com>
2023-05-25 18:53:24 +0200

    Create an explotion effect for the starship when game over

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -27,36 +27,48 @@ public class Player : MonoBehaviour
 
     void Update()
     {
-        /* PLAYER'S CONTROLS */
-        if (Input.GetKey(KeyCode.A) && (transform.position.x >= -sideLimits))
-        {
-            transform.Translate(Vector3.left * Time.deltaTime * moveSpeed, Space.World);
-        }
 
-        if (Input.GetKey(KeyCode.D) && (transform.position.x <= sideLimits))
-        {
-            transform.Translate(Vector3.right * Time.deltaTime * moveSpeed, Space.World);
-        }
+        text.GetComponentInChildren<TextMeshProUGUI>().text = "Score: " + score + " / Lives: " + currentLives;
 
-        /* PLAYER'S LASER */
-        if (Input.GetMouseButtonDown(0) || Input.GetKeyDown(KeyCode.Space))
+        if (currentLives > 0)
         {
-            GameObject newlaser = Instantiate(laser);
-            newlaser.transform.position = new Vector3(transform.position.x, transform.position.y, 1.50f);
-            newlaser.SetActive(true);
-            newlaser.AddComponent<Laser>();
+            /* PLAYER'S CONTROLS */
+            if (Input.GetKey(KeyCode.A) && (transform.position.x >= -sideLimits))
+            {
+                transform.Translate(Vector3.left * Time.deltaTime * moveSpeed, Space.World);
+            }
+
+            if (Input.GetKey(KeyCode.D) && (transform.position.x <= sideLimits))
+            {
+                transform.Translate(Vector3.right * Time.deltaTime * moveSpeed, Space.World);
+            }
+
+            /* PLAYER'S LASER */
+            if (Input.GetMouseButtonDown(0) || Input.GetKeyDown(KeyCode.Space))
+            {
+                GameObject newlaser = Instantiate(laser);
+                newlaser.transform.position = new Vector3(transform.position.x, transform.position.y, 1.50f);
+                newlaser.SetActive(true);
+                newlaser.AddComponent<Laser>();
+            }
         }
 
         /* PLAYER'S LOSE CONDITION */
-        text.GetComponentInChildren<TextMeshProUGUI>().text = "Score: " + score + " / Lives: " + currentLives;
-        if (currentLives <= 0)
+        else
         {
             if (score > StaticVariables.scores[StaticVariables.activeUser])
             {
                 StaticVariables.scores[StaticVariables.activeUser] = score;
             }
-            SceneManager.LoadScene(0);
+            gameObject.GetComponent<Renderer>().enabled = false;
+            gameObject.GetComponent<ParticleSystem>().Play();
+            Invoke("gameOver", gameObject.GetComponent<ParticleSystem>().main.startLifetime.constant - 0.2f);
         }
+    }
+
+    void gameOver()
+    {
+        SceneManager.LoadScene(0);
     }
 
     public void loseLife()
